67eae2ec2e1bf17aba1dd13a876038c074d36f4b
Venki <venki.suragani@shushmoney.com>
2021-05-17 15:13:37 +0100

    connection string

--- src/dao/get-hello.ts
+++ src/dao/get-hello.ts
@@ -1,25 +1,13 @@
 import { sequelizeConnection } from "src/helpers/dbConnectionHelper";
 
 
-export const helloData  = async (): Promise<any> => {
-
-
+export const helloData  = async (): Promise<any> => 
  
     await sequelizeConnection.authenticate();
     console.log('Connection has been established successfully.');
     sequelizeConnection.query("SELECT * FROM emp_data", { type: sequelizeConnection.QueryTypes.SELECT})
 
-    // return new Promise((resolve: any, reject: any) => {
-    //     try {
-       
-    //         resolve({
-    //             "name": "test",
-    //             "value": "any"
-    //           });
-    //     } catch (ex) {
-    //         reject("error");
-    //     }
-    // });
+  
 
 
 
