5980408cb587ce26c2fd1c5ceada1ddc51713885
Venki <venki.suragani@shushmoney.com>
2021-05-17 15:11:31 +0100

    conection string

--- src/dao/get-hello.ts
+++ src/dao/get-hello.ts
@@ -1,16 +1,27 @@
+import { sequelizeConnection } from "src/helpers/dbConnectionHelper";
 
-export const helloData  = (): Promise<any> => {
-   
-    return new Promise((resolve: any, reject: any) => {
-        try {
-            resolve({
-                "name": "test",
-                "value": "any"
-              });
-        } catch (ex) {
-            reject("error");
-        }
-    });
+
+export const helloData  = async (): Promise<any> => {
+
+
+ 
+    await sequelizeConnection.authenticate();
+    console.log('Connection has been established successfully.');
+    sequelizeConnection.query("SELECT * FROM emp_data", { type: sequelizeConnection.QueryTypes.SELECT})
+
+    // return new Promise((resolve: any, reject: any) => {
+    //     try {
+       
+    //         resolve({
+    //             "name": "test",
+    //             "value": "any"
+    //           });
+    //     } catch (ex) {
+    //         reject("error");
+    //     }
+    // });
+
+
 
   
 }
---
+++ src/helpers/dbConnectionHelper.ts
@@ -0,0 +1,7 @@
+const { Sequelize } = require('sequelize');
+
+
+export const sequelizeConnection = new Sequelize('dev', 'dbadmin', '^2C!Yi4HP^*59r8mfvA#', {
+  host: 'shush-dev.cluster-c4c2sa6jb2rt.eu-west-2.rds.amazonaws.com',
+  dialect: 'postgres' 
+});
